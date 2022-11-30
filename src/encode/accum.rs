2c0e07e348d9e170823d5a2470def266ae181d08
sdual <shr.nrht.0623@gmail.com>
2022-11-30 21:47:42 +0900

    blend probabilities

--- src/encode/accum.rs
+++ src/encode/accum.rs
@@ -1,26 +1,25 @@
 pub struct PosteriorProbAccumulator {
+    pub total_count: i64,
     target_value_count: i64,
-    target_value: String,
 }
 
 impl PosteriorProbAccumulator {
-    pub fn new(target_value: String) -> Self {
+    pub fn new() -> Self {
         PosteriorProbAccumulator {
+            total_count: 0,
             target_value_count: 0,
-            target_value,
         }
     }
 
-    pub fn prob(&self, total_count: i64) -> f32 {
-        if total_count == 0 {
-            0.0
-        } else {
-            (self.target_value_count as f64 / total_count as f64) as f32
-        }
+    pub fn prob(&self) -> f32 {
+        (self.target_value_count as f64 / self.total_count as f64) as f32
     }
 
-    pub fn increment(&mut self) {
-        self.target_value_count += 1;
+    pub fn increment(&mut self, target: i32) {
+        self.total_count += 1;
+        if target == 1 {
+            self.target_value_count += 1;
+        }
     }
 }
 
--- src/encode/factor.rs
+++ src/encode/factor.rs
@@ -1,3 +1,3 @@
-pub fn shrinkage_factor(num_case: i32, param: f32) -> f32 {
-    num_case as f32 / (num_case as f32 + param)
+pub fn shrinkage_factor(num_case: i64, param: f32) -> f32 {
+    (num_case as f64 / (num_case as f64 + param as f64)) as f32
 }
